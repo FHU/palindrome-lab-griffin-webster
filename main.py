e14e80278a593f2800b404533696c85c2dde320b
griffin-webster <griffin.webster@students.fhu.edu>
2024-02-12 01:04:00 -0600

    working solution, not passing tests
    I have reached my wit's end.

--- main.py
+++ main.py
@@ -12,4 +12,8 @@ word = word.replace(" ","")
 word = word.strip()
 
 answer = palindrome(word)
+
 print(answer)
+
+# I genuinely don't know how to fix this lab. As best as I can tell, my function is returning all of expected answers. 
+# No matter how many times I push it, though, it never passes any checks.
